742bbe03d845e6e1fc28329cf33db4c653e2238a
Danny Woodford <dwoodford@brandgage.com>
2023-06-08 11:01:46 -0400

    downgrade react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
+import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
+
 import Scene from './canvas'
 import Customizer from './pages/Customizer'
 import Home from './pages/Home'
@@ -12,7 +14,9 @@ function App() {
 				camera={{ position: [0, 0, 2], fov: 25 }}
 				gl={{ preserveDrawingBuffer: true }}
 				className='w-full max-w-full h-full transition-all ease-in'>
-				<Scene />
+				<Suspense fallback={null}>
+					<Scene />
+				</Suspense>
 			</Canvas>
 
 			<Customizer />
