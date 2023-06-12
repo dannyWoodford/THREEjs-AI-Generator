cba5286f72aa4f7e4fafb192f897b9317c8030e9
Danny Woodford <dwoodford@brandgage.com>
2023-06-12 10:16:37 -0400

    commit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import generateCode from './routes/generateCode.routes.js'
-import dalleRoutes from './routes/dalle.routes.js';
 
 dotenv.config();
 
@@ -12,7 +11,6 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }))
 
 app.use('/v1/chat/completions', generateCode)
-app.use("/api/v1/dalle", dalleRoutes);
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: "Hello from DALL.E" })
